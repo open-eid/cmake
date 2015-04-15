c4b118bbdbf90d1fdc78ad693c7231748592102d
Raul Metsma <raul@metsma.ee>
2015-04-15 11:41:13 +0300

    Increase version number to 3.11.0

--- modules/VersionInfo.cmake
+++ modules/VersionInfo.cmake
@@ -1,6 +1,6 @@
 set( MAJOR_VER 3 )
-set( MINOR_VER 10 )
-set( RELEASE_VER 3 )
+set( MINOR_VER 11 )
+set( RELEASE_VER 0 )
 set( BUILD_VER 0 )
 if( $ENV{BUILD_NUMBER} )
 	set( BUILD_VER $ENV{BUILD_NUMBER} )
